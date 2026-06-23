3bbfce6a2540b5dfddce16c0f26e12de7710ed78
AxelMardiyo <axel.mardiyo1006@gmail.com>
2026-06-23 09:59:27 +0700

    feat: add Python lessons on  expressions and oeprators

---
+++ ekspresi-uner.py
@@ -0,0 +1,26 @@
+a = True
+a = not a
+print(a)
+
+b = 6
+b -= 1
+print(b)
+
+c = 6
+c += 1
+print(c)
+
+d = 10
+print(-d)
+
+# ekspresi menurut tipe data yang dihasilkan
+# Ekspresi Aritmetika <numerik> <operator> <numerik> = <numerik>:
+print(2 + 2)
+print(2 * 2)
+
+# Ekspresi Relasional <numerik> <operator> <numerik> = <boolean>:
+print(3 < 10)
+print(1 > 10)
+
+# Ekspresi Logika <boolean> <operator> <boolean> = <boolean>:
+print(True and False)
---
+++ operator.py
@@ -0,0 +1,72 @@
+# Operator Aritmetika
+x = 11
+y = 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x // y)
+print(x / y)
+print(x % y)
+print(x ** y)
+
+# Operator Relasional
+x = 5
+y = 10
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+x = "Dicoding"
+y = "Indonesia"
+
+print(x == y) # True jika sama persis
+print(x != y) # True jika tidak bernilai sama
+print(x > y) # True jika huruf pertama x memiliki nilai unicode dgn urutan lebih tinggi
+print(x < y) # True jika huruf pertama x memiliki nilai unicode dgn urutan lebih rendah
+print(x >= y) # sm aja
+print(x <= y) # sm aja
+
+# Operator Logika
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print(not True)
+print(not False)
+
+# Operator Assignment
+# +=
+x = 11
+x += 5
+print(x)
+
+# -=
+x = 11
+x -= 5
+print(x)
+
+# *=
+x = 11
+x *= 5
+print(x)
+
+# /=
+x = 11
+x /= 5
+print(x)
+
+#%=
+x = 11
+x%= 5
+print(x)
